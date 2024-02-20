17501650a985b12ba28f4da5376f4f9b6188c997
eduardo <eduard_snake2007@hotmail.com>
2024-02-20 10:14:38 -0300

    primera version de nuestro proyecto

---
+++ script.js
@@ -0,0 +1,34 @@
+/*const nombre="matias figueroa"
+const edad =25
+
+const puede_votar=edad>18
+const locura=125==5**3
+
+//le pregunto al usuario come se llama
+const nombre= prompt("hola,como te llamas?")
+
+//creo un string con el saludo
+const saludo= "hola, " + nombre + " encantado de conocerte"
+ 
+
+//le mostramos el saludo al usuario
+
+alert(saludo)
+*/
+
+
+ let usuario =prompt("ingrese edad")
+ //transformar string a numero
+ usuario=parseInt(usuario)
+if(usuario<18){
+    alert("no puede hacer la fila")
+}else if(usuario!=number){
+    alert("debe ingresar numero")
+}else{
+    alert("puede hacer la fila")
+}
+
+/*
+const programa=usuario+1
+ const frase ="yo gane con el " +programa
+alert(frase)*/
